83179fa58aae113fe810e9a73680fb881a3b43b9
Jan Rudolf <rudolf.j.j@gmail.com>
2018-08-03 11:48:30 +0200

    Initial commit

---
+++ app/src/main/java/cz/janrudolf/practiceandroidapplication/MainActivity.kt
@@ -0,0 +1,15 @@
+package cz.janrudolf.practiceandroidapplication
+
+import android.app.Activity
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by jrudolf on 3.8.18.
+ */
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
